e09dc0c436fcbd6ada4cedf3443fff3b358a3325
Igarevv <naturelovee12@gmail.com>
2025-05-29 22:13:34 +0300

    media

---
+++ resources/js/utils/availableLanguages.ts
@@ -0,0 +1,9 @@
+import {Language} from "@Modules/Auth/resources/views/components/Registration/Steps/UserPreferencesForm.vue";
+
+const languages: Language[] = [
+    { languageCode: 'en', languageName: 'English' },
+    { languageCode: 'ru', languageName: 'Русский' },
+    { languageCode: 'ua', languageName: 'Українська' },
+];
+
+export default languages;
