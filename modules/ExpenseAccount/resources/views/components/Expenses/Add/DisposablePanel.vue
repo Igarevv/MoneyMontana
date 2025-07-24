4117a2355e28b1a29ea3255a1075ec833c88fe17
Igarevv <naturelovee12@gmail.com>
2025-07-24 22:46:22 +0300

    disposable panel for expense add sidebar #2

--- modules/ExpenseAccount/resources/views/components/Expenses/Add/DisposablePanel.vue
+++ modules/ExpenseAccount/resources/views/components/Expenses/Add/DisposablePanel.vue
@@ -1,16 +1,55 @@
 <script setup lang="ts">
 import InputNumber from "primevue/inputnumber";
 import FloatLabel from "primevue/floatlabel";
+import {useAllCountriesInfo} from "@/composables/useAllCountriesInfo";
+import {onMounted, ref} from "vue";
+import {usePreferences} from "@/composables/usePreferences";
+import Select from "primevue/select";
+import DatePicker from "primevue/datepicker";
+
+const {loadCountries, currencies} = useAllCountriesInfo();
+
+const date = ref<Date | null>(new Date());
+
+const selectedCurrency = ref(usePreferences().currency);
+
+const isFutureDate = (date: Date | null) => {
+  if (!date) {
+    return false;
+  }
+
+  const today = new Date();
+
+  return date.getTime() > today.getTime();
+}
+
+onMounted(() => {
+  loadCountries();
+})
 </script>
 
 <template>
-  <div class="flex">
-    <FloatLabel variant="on">
-      <InputNumber inputId="on_label" mode="currency" currency="USD" locale="en-US"/>
-      <label for="on_label">Amount</label>
-    </FloatLabel>
-    <Select v-model="selectedCity" :options="cities" optionLabel="name" placeholder="Select a City"
-            class="w-full md:w-56"/>
+  <div class="flex flex-col gap-8">
+    <div class="flex gap-4">
+      <FloatLabel variant="on" class="w-1/2">
+        <InputNumber inputId="on_label" mode="currency" :currency="selectedCurrency" locale="en-US" class="w-full"/>
+        <label for="on_label">{{ $t('panel.expense_accounting.all_expenses.add_form.amount_label') }}</label>
+      </FloatLabel>
+      <Select v-model="selectedCurrency" :options="currencies" class="w-28"/>
+    </div>
+
+    <div class="flex gap-4 items-center">
+      <FloatLabel variant="on">
+        <DatePicker v-model="date" inputId="date" showIcon iconDisplay="input"/>
+        <label for="date">{{ $t('panel.expense_accounting.all_expenses.add_form.expense_disposable.date') }}</label>
+      </FloatLabel>
+
+      <span
+          v-if="isFutureDate(date)"
+          class="text-xs">{{
+          $t('panel.expense_accounting.all_expenses.add_form.expense_disposable.date_in_future')
+        }}</span>
+    </div>
   </div>
 </template>
 
